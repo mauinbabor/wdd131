dd3c616be734ee3f7f3799f6ed71e631526d809f
mauin babor <mauinbabor@gmail.com>
2025-01-26 22:11:10 +0800

    finish

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,12 @@
+// Dynamically Update Footer
+document.getElementById('2025').textContent = new Date().getFullYear();
+document.getElementById('lastModified:01/23/2025').textContent = document.lastModified;
+
+// Hamburger Menu Toggle
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('nav ul');
+const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    hamburger.textContent = navMenu.classList.contains('active') ? '✖' : '☰';
 });
-
-document.getElementById('year').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
