6c1684cfc9d20925446354f77c55527d1034bb36
mauinbabor <168878273+mauinbabor@users.noreply.github.com>
2025-01-26 22:44:11 +0800

    Delete scripts/getdates.js

--- scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-// Dynamically update the current year in the footer
-const currentYearSpan = document.getElementById('2025');
-const currentYear = new Date().getFullYear(); // Get the current year
-currentYearSpan.textContent = currentYear; // Set it to the span element
-
-// Dynamically update the last modified date in the footer
-const lastModifiedParagraph = document.getElementById('lastModified');
-lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
-
